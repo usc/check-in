4a71e4d5534c238069db5cea07fbea1381b1e00b
lishunli <lishunli.me@gmail.com>
2015-11-07 13:34:50 +0800

    add switch for every checkin

--- src/main/java/org/usc/check/in/task/BaseTask.java
+++ src/main/java/org/usc/check/in/task/BaseTask.java
@@ -20,14 +20,17 @@ public abstract class BaseTask {
     protected abstract String name();
 
     protected List<Account> buildAccounts() {
+        String keyPrefix = name();
+
         List<Account> accounts = new ArrayList<Account>();
+        if (config.getBoolean(keyPrefix + ".switch", true)) {
+            List<HierarchicalConfiguration> configurationsAt = config.configurationsAt(keyPrefix + ".accounts.account");
+            for (HierarchicalConfiguration hierarchicalConfiguration : configurationsAt) {
+                String username = hierarchicalConfiguration.getString("username");
+                String password = hierarchicalConfiguration.getString("password");
 
-        List<HierarchicalConfiguration> configurationsAt = config.configurationsAt(name() + ".accounts.account");
-        for (HierarchicalConfiguration hierarchicalConfiguration : configurationsAt) {
-            String username = hierarchicalConfiguration.getString("username");
-            String password = hierarchicalConfiguration.getString("password");
-
-            accounts.add(new Account(username, password));
+                accounts.add(new Account(username, password));
+            }
         }
 
         return accounts;
