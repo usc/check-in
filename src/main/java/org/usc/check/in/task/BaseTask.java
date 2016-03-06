8b0782c053ecc56cc8d31be4bc5096e7ad168120
lishunli <lishunli.me@gmail.com>
2016-03-06 12:13:41 +0800

    encrypt password with des, encryptPassword = DesUtil.encrypt(password, parentName + username)

--- src/main/java/org/usc/check/in/task/BaseTask.java
+++ src/main/java/org/usc/check/in/task/BaseTask.java
@@ -8,6 +8,7 @@ import org.apache.commons.configuration.XMLConfiguration;
 import org.apache.http.client.fluent.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.usc.check.in.model.Account;
+import org.usc.check.in.util.DesUtil;
 
 /**
  *
@@ -26,16 +27,19 @@ public abstract class BaseTask {
         if (config.getBoolean(keyPrefix + ".switch", true)) {
             List<HierarchicalConfiguration> configurationsAt = config.configurationsAt(keyPrefix + ".accounts.account");
             for (HierarchicalConfiguration hierarchicalConfiguration : configurationsAt) {
-                String username = hierarchicalConfiguration.getString("username");
-                String password = hierarchicalConfiguration.getString("password");
+                try {
+                    String username = hierarchicalConfiguration.getString("username");
+                    String encryptPassword = hierarchicalConfiguration.getString("password");
+                    String password = DesUtil.decrypt(encryptPassword, keyPrefix + username);
 
-                accounts.add(new Account(username, password));
+                    accounts.add(new Account(username, password));
+                } catch (Exception e) {
+                }
             }
         }
 
         return accounts;
     }
-
     protected Request appendTimeOuts(Request request) {
         return request.connectTimeout(config.getInt("timeouts.connect", 60000)).socketTimeout(config.getInt("timeouts.socket", 60000));
     }
